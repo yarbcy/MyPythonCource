16838f906506184450aa5f8446e7941450335907
yaremkivb <yaremkivb@gmail.com>
2019-05-01 13:45:58 +0300

    Complete task1

--- home_tasks/lesson3/task1.py
+++ home_tasks/lesson3/task1.py
@@ -3,9 +3,11 @@ zen = io.StringIO()
 with contextlib.redirect_stdout(zen):
     import this
 
+zen_str = str(zen.getvalue())
+
 def countWord(word):
     count=0
-    for item in zen.getvalue().split():
+    for item in zen_str.split():
         if item == word:
             count+=True
     return count
@@ -14,3 +16,12 @@ print(countWord('is'))
 print(countWord('better'))
 print(countWord('never'))
 
+print(zen_str.upper())
+
+while (zen_str.find('I')>=0) or (zen_str.find('i')>=0):
+    zen_str = zen_str.replace('I','&')
+    zen_str = zen_str.replace('i','&')
+
+print(zen_str)
+    
+    
