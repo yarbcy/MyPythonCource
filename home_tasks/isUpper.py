28dba4d371d57dac8c790360b04c5bbb2c21c906
yaremkivb <yaremkivb@gmail.com>
2019-06-02 16:22:01 +0300

    Add new home task

---
+++ home_tasks/isUpper.py
@@ -0,0 +1,15 @@
+# Create a method is_uppercase() to see whether the string is ALL CAPS. For example:
+# is_uppercase("c") == False
+# is_uppercase("C") == True
+# is_uppercase("hello I AM DONALD") == False
+# is_uppercase("HELLO I AM DONALD") == True
+# is_uppercase("ACSKLDFJSgSKLDFJSKLDFJ") == False
+# is_uppercase("ACSKLDFJSGSKLDFJSKLDFJ") == True
+
+def is_uppercase(inp):
+    for i in inp:
+        if i.isalpha():
+            if not i.isupper():
+                return False
+                break
+    return True
