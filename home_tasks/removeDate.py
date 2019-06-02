b80e8b034bc322fd7001998af15f796a4373b0cf
yaremkivb <yaremkivb@gmail.com>
2019-06-02 17:12:55 +0300

    Add new home task

---
+++ home_tasks/removeDate.py
@@ -0,0 +1,9 @@
+#Test.describe("Basic tests")
+#Test.assert_equals(shorten_to_date("Monday February 2, 8pm"), "Monday February 2")
+#Test.assert_equals(shorten_to_date("Tuesday May 29, 8pm"), "Tuesday May 29")
+#Test.assert_equals(shorten_to_date("Wed September 1, 3am"), "Wed September 1")
+#Test.assert_equals(shorten_to_date("Friday May 2, 9am"), "Friday May 2")
+#Test.assert_equals(shorten_to_date("Tuesday January 29, 10pm"), "Tuesday January 29")
+
+def shorten_to_date(long_date):    
+    return long_date[:long_date.find(',')]
